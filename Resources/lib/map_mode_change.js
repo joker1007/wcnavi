1b6f9864cb9157e878b12c81532ae1caa0b6889e
joker1007 <kakyoin.hierophant@gmail.com>
2011-05-14 02:40:45 +0900

    add function mapModeChange

---
+++ Resources/lib/map_mode_change.js
@@ -0,0 +1,43 @@
+var guide_label = null;
+
+function createLabel() {
+	guide_label = Titanium.UI.createLabel({
+		text:'投稿したい駅を選択してください',
+		textAlign:'center',
+		width:'auto',
+		font:{fontSize:20},
+		backgroundColor:'#fff',
+		color:'#336699',
+		shadowColor:'#bbb',
+		shadowOffset:{x:2,y:2},
+		top:338
+	});
+	Titanium.UI.currentWindow.add(guide_label);
+}
+
+function mapModeEdit() {
+	tableview.animate({visible:false, duration:100}, function(e) {
+		mapview.animate({height:338, duration:300}, function(e) {
+			createLabel();
+		});
+	});
+}
+
+function tableShow(tb) {
+	tb.show();
+}
+
+function mapShrink(map) {
+	map.animate({height:200, duration:300}, function(e) {
+		tableShow(tableview);
+	});
+}
+
+function labelHide(label) {
+	label.hide();
+}
+
+function mapModeNormal() {
+	labelHide(guide_label);
+	mapShrink(mapview);
+}
